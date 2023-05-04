e76b7946eda88e849c552eddd2f591af219dd603
Fatuma Meshalla <mompround@gmail.com>
2023-05-03 19:17:44 -0700

    change

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const faker = require("faker");
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 8000;
 // console.log(faker.datatype.uuid());
 // console.log(faker.phone.phoneNumber())
 // console.log(faker.name.firstName())
 // console.log(faker.name.lastName())
-
 // users object 
 const userObject =()=>({
     _id :faker.datatype.uuid(),
@@ -29,29 +28,26 @@ const companyObject = ()=>({
 console.log(companyObject())
 // Create an api route "/api/users/new" that returns a new user
 app.get("/api/users/new ", (_req,res) => {
-    const newUser = newUserInfo();
+    const newUser = userObject();
     //  let user = new User();
     res.json(newUser)
     console.log(newUser)
     
 });
-
 // // Create an api route "/api/companies/new" that returns a new company
-app.get("/api/companies/new", (_req,res)=>{
-    const  newCompany = newCompanyInfo();
-    res.json({company})
+app.get("/api/companies/new", (req,res)=>{
+    const  newCompany = companyObject();
+    // req.body will contain the form data from Postman or from React
+    // console.log(newCompany)
+    res.json({newCompany})
+    console.log(newCompany)
 })
 // Create an api route "/api/user/company" that returns both a new user and a new company
 app.get("/api/user/company", (_req,res)=>{
-    const  newCompany = newCompanyInfo();
-    const NewUser = newUsers();
-    res.json({newCompany,newUser})
+    const  newCompany = companyObject();
+    const newUser = userObject();
+    res.json({newCompany , newUser})
 })
-
-
-
-
-
 app.listen(port,()=>console.log(`Listening on port: ${port}`))
 
 
